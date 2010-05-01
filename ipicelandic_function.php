af2f2627ba359e825d5d700355b504525cd4e543
Gunni <gunni@wave.is>
2010-05-01 00:18:53 +0000

    Changed: Separated example into it's own file.

--- ipicelandic_function.php
+++ ipicelandic_function.php
@@ -161,19 +161,4 @@ function ipicelandic($ip)
     
     return $is_icelandic;
 }
-
-// USAGE EXAMPLE:
-$address = $_SERVER['REMOTE_ADDR'];
-
-if (ipicelandic($address) == false)
-{
-    echo "The IP address $address is not icelandic. A nuclear bomb has been dispatched and is now en route towards you.\n";
-}
-else
-{
-    echo "The IP address $address is icelandic. Yay!\n";
-}
-
-// You can now do what ever you want with the result.
-
 ?>
